a0874d2e43cc3bd95b0806246d4cc86ad8161f20
muizayo1 <muiz1ayo@gmail.com>
2021-02-20 03:01:06 -1100

    com2

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Icon1 from '../../images/svg-1.svg';
-import Icon2 from '../../images/svg-3.svg';
+import Icon2 from '../../images/svg-3.svg'
 import Icon3 from '../../images/svg-5.svg';
 import { ServicesContainer,ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon, ServicesH2,ServicesP } from "./ServiceElements"
 
